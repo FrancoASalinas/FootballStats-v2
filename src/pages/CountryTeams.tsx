e650c07977e8398e5c86d1b8d6ce6fbcb396fa8c
FrancoSMiño <frankow222@gmail.com>
2023-07-14 15:48:36 -0300

    improved countries/teams population

--- src/pages/CountryTeams.tsx
+++ src/pages/CountryTeams.tsx
@@ -3,17 +3,42 @@ import { Link, useLoaderData } from 'react-router-dom';
 function CountryTeams() {
   const data: any = useLoaderData();
 
+  let letter = '';
+
+  const sortedData = data.response.toSorted((a: any, b: any) => {
+    if (a.team.name > b.team.name) {
+      return +1;
+    } else if (a.team.name < b.team.name) {
+      return -1;
+    } else return 0;
+  });
+
+
   return (
     <>
       <h2></h2>
-      <ul>
-        {data.response.length > 0 &&
-          data.response.map((item: any) => (
-            <li key={item.team.id}>
-              <Link to={`/team/${item.team.id}`}>{item.team.name}</Link>
-            </li>
-          ))}
-      </ul>
+      <div className="sm:grid grid-cols-2 ">
+        {sortedData.length > 0 &&
+          sortedData.map((item: any) => {
+            if (item.team.name[0] === letter) return;
+            letter = item.team.name[0];
+
+            return (
+              <div key={letter + 1} className="py-5 text-xl">
+                <h3 className="text-2xl">{letter}</h3>
+                <ul>
+                  {sortedData
+                    .filter((it: any) => it.team.name[0] === letter)
+                    .map((i: any) => (
+                      <li key={i.team.id}>
+                        <Link to={`/team/${i.team.id}`}>{i.team.name}</Link>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 }
--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -4,12 +4,18 @@ function Team() {
   const data: any = useLoaderData();
 
   return (
-    <>
-      <h1>{data.response[0].team.name}</h1>
-      <ul>
-        <li>sdasdasdasd</li>
-      </ul>
-    </>
+    <div className="p-5">
+      <h1 className="text-3xl ">{data.response[0].team.name}</h1>
+      <img className="w-20" src={data.response[0].team.logo}></img>
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+      <article></article>
+    </div>
   );
 }
 
