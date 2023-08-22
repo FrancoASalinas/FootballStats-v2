608badaa2c5f9646d1da5ad0aee1e33d31265d11
FrancoSMiño <frankow222@gmail.com>
2023-08-22 19:55:33 -0300

    Improved various performance aspects.

--- src/modules/Header.tsx
+++ src/modules/Header.tsx
@@ -14,12 +14,14 @@ function Header() {
         <Link to="/">FS</Link>
       </h2>
       <button onClick={handleNav}>
-        <i className="fa-solid fa-bars sm:hidden sm:w-0 sm:h-0"></i>
+        <Bars />
       </button>
       <MobileNav nav={nav} handleNav={handleNav} />
       <Nav/>
     </header>
   );
 }
+
+const Bars = () => <svg className='sm:w-0 sm:h-0 sm:hidden' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
 
 export default Header;
--- src/modules/Nav.tsx
+++ src/modules/Nav.tsx
@@ -3,6 +3,7 @@ import useDarkTheme from "../utils/useDarkTheme";
 import useInstallPrompt from "../utils/useInstallPrompt";
 import Slider from "./Slider";
 import useOfflineMode from "../utils/useOfflineMode";
+import { Moon, Sun } from "./MobileNav";
 
 function Nav() {
 
@@ -30,9 +31,9 @@ function Nav() {
           </li>
           <li className="p-3">
             <Slider checked={darkTheme} onChange={setDarkTheme}>
-              <i className="fa-solid fa-moon absolute top-1/2 -translate-y-1/2 left-1 z-10 opacity-0 peer-checked:opacity-100 duration-[.2s] text-light"></i>
+              <Moon/>
               <span className="absolute cursor-pointer peer-checked:bg-dark top-0 left-0 right-0 bottom-0 bg-light duration-[.2s] rounded-full before:absolute before:h-[26px] before:w-[26px] before:left-[4px] before:bottom-[4px] before:bg-primary before:duration-[.2s] peer-checked:before:translate-x-full before:rounded-full"></span>
-              <i className="fa-solid fa-sun absolute top-1/2 -translate-y-1/2 right-1 z-10 opacity-100 peer-checked:opacity-0 duration-[.2s] text-dark "></i>
+              <Sun />
             </Slider>
           </li>
           <li className="p-3 flex gap-2 items-center">
